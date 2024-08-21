0eed1078ac46f70bb1775477f40f0c502748f8a9
Osama Mahmoud <ossaamamahmoud@gmail.com>
2024-08-21 19:38:23 +0300

    - Just centered the login screen content.

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -25,64 +25,66 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Welcome Back',
-                  style: TextStyles.font24BlueBold,
-                ),
-                verticalSpace(8),
-                Text(
-                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
-                  style: TextStyles.font14GrayRegular,
-                ),
-                verticalSpace(36),
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      const AppTextFormField(hintText: 'Email'),
-                      verticalSpace(18),
-                      AppTextFormField(
-                        hintText: 'Password',
-                        isObscureText: isObscureText,
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              isObscureText = !isObscureText;
-                            });
-                          },
-                          child: Icon(
-                            isObscureText
-                                ? Icons.visibility_off
-                                : Icons.visibility,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome Back',
+                    style: TextStyles.font24BlueBold,
+                  ),
+                  verticalSpace(8),
+                  Text(
+                    'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                    style: TextStyles.font14GrayRegular,
+                  ),
+                  verticalSpace(36),
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        const AppTextFormField(hintText: 'Email'),
+                        verticalSpace(18),
+                        AppTextFormField(
+                          hintText: 'Password',
+                          isObscureText: isObscureText,
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isObscureText = !isObscureText;
+                              });
+                            },
+                            child: Icon(
+                              isObscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
                           ),
                         ),
-                      ),
-                      verticalSpace(24),
-                      Align(
-                        alignment: AlignmentDirectional.centerEnd,
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyles.font13BlueRegular,
+                        verticalSpace(24),
+                        Align(
+                          alignment: AlignmentDirectional.centerEnd,
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyles.font13BlueRegular,
+                          ),
                         ),
-                      ),
-                      verticalSpace(40),
-                      AppTextButton(
-                        buttonText: "Login",
-                        textStyle: TextStyles.font16WhiteSemiBold,
-                        onPressed: () {},
-                      ),
-                      verticalSpace(16),
-                      const TermsAndConditionsText(),
-                      verticalSpace(60),
-                      const AlreadyHaveAccountText(),
-                    ],
+                        verticalSpace(40),
+                        AppTextButton(
+                          buttonText: "Login",
+                          textStyle: TextStyles.font16WhiteSemiBold,
+                          onPressed: () {},
+                        ),
+                        verticalSpace(16),
+                        const TermsAndConditionsText(),
+                        verticalSpace(60),
+                        const AlreadyHaveAccountText(),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
